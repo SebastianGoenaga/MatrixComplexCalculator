edf6e47ec29276973b783c840934ea14cf108066
Sebastian <sebastian.goenaga@hotmail.com>
2019-10-08 23:01:57 -0500

    innerVector

--- src/main/java/calculator/MatrixCalculator.java
+++ src/main/java/calculator/MatrixCalculator.java
@@ -110,6 +110,11 @@ public class MatrixCalculator {
         return trace(matrixMultiplication(adjointMatrix(m1), m2));
     }
 
+    public static Complex innerProduct(ComplexVector v1, ComplexVector v2) {
+        //noinspection ConstantConditions
+        return innerProduct(adjointMatrix((ComplexMatrix) v1),(ComplexMatrix) v2);
+    }
+
     public static Complex trace(ComplexMatrix m1) {
         if (m1.isSquare()) {
             Complex sum = new Complex(0, 0);
